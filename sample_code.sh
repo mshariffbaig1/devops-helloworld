85f32f125ca635293550640de41a7ad3b5fb0924
mshariffbaig1 <mshariffbaig@outlook.com>
2024-11-05 06:27:02 +0300

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #
-
-echo "This is an Samples Script from GitHub"
-echo "This is an example of a script"
+echo "test file"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
